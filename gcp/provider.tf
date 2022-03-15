c069c3e090c60dd4e4684137810a8571cec46add
Vitaly Nedviga <vitaly.nedviga@macys.com>
2022-03-15 17:05:12 +0200

    move gcp to separate folder

---
+++ gcp/provider.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  region = "${var.region}"
+}
+
+provider "google-beta" {
+  region = var.region
+}
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
