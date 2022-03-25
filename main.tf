7f4178e324965bba186429b8cb7945feffc8eff7
Tom Fabler <imagination.fault@gmail.com>
2022-03-25 21:37:33 +0200

    delete VMs in scale set on schedule

---
+++ main.tf
@@ -0,0 +1,137 @@
+locals {
+  name = "glory-to-ukraine"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = local.name
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "network" {
+  name                = local.name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = local.name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["10.0.99.0/24"]
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vms" {
+  name                = local.name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = var.vm_type
+  instances           = var.vm_count
+  admin_username      = trimspace(basename(pathexpand("~")))
+  custom_data         = "${base64encode(file("${path.module}/script.sh"))}"
+
+  admin_ssh_key {
+    username   = trimspace(basename(pathexpand("~")))
+    public_key = tls_private_key.ssh.public_key_openssh
+  }
+
+  network_interface {
+    name    = local.name
+    primary = true
+
+    ip_configuration {
+      name      = local.name
+      primary   = true
+      subnet_id = azurerm_subnet.internal.id
+
+      public_ip_address {
+        name = local.name
+      }
+    }
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_monitor_autoscale_setting" "glory_to_ukraine" {
+  name                = local.name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.vms.id
+
+  profile {
+    name = local.name
+
+    capacity {
+      default = var.vm_count
+      minimum = var.vm_count
+      maximum = var.vm_count
+    }
+  }
+}
+
+resource "azurerm_automation_account" "glory_to_ukraine" {
+  name                = "glory-to-ukraine"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "Basic"
+  identity {
+     type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_automation_schedule" "hourly" {
+  name                    = local.name
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.glory_to_ukraine.name
+  frequency               = "Hour"
+  interval                = 1
+  start_time              = timeadd(timestamp(), "10m")
+  description             = "Hourly schedule"
+}
+
+data "template_file" "init" {
+  template = "${file("${path.module}/deleteVMs.tpl")}"
+  vars = {
+    local_name = "${local.name}"
+  }
+}
+
+resource "azurerm_automation_runbook" "delVMs" {
+  name                    = "delVMs"
+  location                = azurerm_resource_group.main.location
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.glory_to_ukraine.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "Delete VMs from VMSS runbook"
+  runbook_type            = "PowerShell"
+
+  content = data.template_file.init.rendered
+}
+
+data "azurerm_subscription" "primary" {
+}
+
+resource "azurerm_role_assignment" "main" {
+  scope              = data.azurerm_subscription.primary.id
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id       = azurerm_automation_account.glory_to_ukraine.identity[0].principal_id
+}
+
+resource "azurerm_automation_job_schedule" "main" {
+  resource_group_name     = azurerm_resource_group.main.name
+  automation_account_name = azurerm_automation_account.glory_to_ukraine.name
+  schedule_name           = azurerm_automation_schedule.hourly.name
+  runbook_name            = azurerm_automation_runbook.delVMs.name
+}
